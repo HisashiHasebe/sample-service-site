35090a14ff5c91412c7e4193ee0ac4a33cf44988
HisashiHasebe <hhasebe@diverta.co.jp>
2024-06-06 11:33:59 +0800

    add: dev branch settings

---
+++ env.development.js
@@ -0,0 +1,9 @@
+module.exports = {
+  API_BASE: 'https://sample-service-site.g.kuroco.app',
+  SITE_TITLE: '[開発]|Kurocoサンプルサイト',
+  META_TITLE: '[開発]|Kurocoサンプルサイト',
+  SITE_DESCRIPTION: '[開発]|Kurocoビギナーズガイドを参考に作成したサンプルサイトです。',
+  RSS_SITE_TITLE: '[開発]|Kurocoサンプル',
+  RSS_SITE_DESCRIPTION: '[開発]|KurocoビギナーズガイドでKuroco利用を開始しましょう',
+  ROBOTS: 'noindex',
+}
