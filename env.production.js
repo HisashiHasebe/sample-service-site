2c312bf911390bcce8be7c2ff4f5c8aab9bde26a
HisashiHasebe <hhasebe@diverta.co.jp>
2024-06-06 11:02:12 +0800

    add:環境変数

---
+++ env.production.js
@@ -0,0 +1,9 @@
+module.exports = {
+    API_BASE: 'https://sample-service-site.g.kuroco.app',
+    SITE_TITLE: 'Kurocoサンプルサイト',
+    META_TITLE: 'Kurocoサンプルサイト',
+    SITE_DESCRIPTION: 'Kurocoビギナーズガイドを参考に作成したサンプルサイトです。',
+    RSS_SITE_TITLE: 'Kurocoサンプル',
+    RSS_SITE_DESCRIPTION: 'KurocoビギナーズガイドでKuroco利用を開始しましょう',
+    ROBOTS: 'index',
+}
---
+++ env.staging.js
@@ -0,0 +1,9 @@
+module.exports = {
+  API_BASE: 'https://sample-service-site.g.kuroco.app',
+  SITE_TITLE: '[ステージング]|Kurocoサンプルサイト',
+  META_TITLE: '[ステージング]|Kurocoサンプルサイト',
+  SITE_DESCRIPTION: '[ステージング]|Kurocoビギナーズガイドを参考に作成したサンプルサイトです。',
+  RSS_SITE_TITLE: '[ステージング]|Kurocoサンプル',
+  RSS_SITE_DESCRIPTION: '[ステージング]|KurocoビギナーズガイドでKuroco利用を開始しましょう',
+  ROBOTS: 'noindex',
+}
